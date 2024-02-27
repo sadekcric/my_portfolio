5719033c91a066ecc77d3967f99e46da3ee3b45b
sadekcric4 <sadekcric4@gmail.com>
2024-02-27 16:58:02 +0600

    js type added

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-const mobileMenu=document.getElementById(`mobile_menu`)
+const mobileMenu = document.getElementById(`mobile_menu`);
 
-mobileMenu.addEventListener(`click`, function(){
-  let list=document.getElementById(`mobile_list`);
-  list.classList.toggle(`mobile_list_item`)
-})
+mobileMenu.addEventListener(`click`, function () {
+  let list = document.getElementById(`mobile_list`);
+  list.classList.toggle(`mobile_list_item`);
+});
+
+let typed = new Typed(".span", {
+  strings: ["Web Developer", "Frontend Developer"],
+  typeSpeed: 150,
+  backSpeed:150,
+  loop:true
+});
